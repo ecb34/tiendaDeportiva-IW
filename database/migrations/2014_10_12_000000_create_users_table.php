97e0f3aef53e613ac0a493ef983148b10f1f2f38
ecb34 <ecb34@alu.ua.es>
2019-11-20 18:02:14 +0100

    creadas migraciones iniciales

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,12 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
+            $table->string('username')->unique();
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('phone');
             $table->string('email')->unique();
+            $table->string('password');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
