abcc1fc9554df586606e3f20eb1611783a8e3ed8
ecb34 <ecb34@alu.ua.es>
2019-11-22 21:00:08 +0100

    añadida migracion lista deseos

---
+++ database/migrations/2019_11_22_195440_create_lista_deseos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateListaDeseosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lista_deseos', function (Blueprint $table) {
+            $table->integer('usuario_id')->unsigned();
+            $table->foreign('usuario_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->integer('articulo_id')->unsigned();
+            $table->foreign('articulo_id')->references('id')->on('articulos')->onDelete('cascade')->onUpdate('cascade');
+            $table->primary(['usuario_id','articulo_id']);
+            $table->timestamps(); //puede ser usado para saber fecha en la que se añadio el producto
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lista_deseos');
+    }
+}
