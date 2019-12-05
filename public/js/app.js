f169d5089ebbb44917cd2b869a8ddec8ad6aa8f0
juanbeteta <jmbt5@alu.ua.es>
2019-12-05 12:15:12 +0100

    header sin footer

--- public/js/app.js
+++ public/js/app.js
@@ -91210,8 +91210,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\laragon\www\tiendaDeportiva\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\laragon\www\tiendaDeportiva\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\JuanMiguelBetetaTumb\Desktop\UNI\Cuarto\IW\Practica2\tiendaDeportiva-IW\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\JuanMiguelBetetaTumb\Desktop\UNI\Cuarto\IW\Practica2\tiendaDeportiva-IW\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
