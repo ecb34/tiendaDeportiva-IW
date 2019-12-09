e7508f6f0dab1e1d7b8c9c5dea0da3d309be5522
Brian Methias Pesci Juliani <bmpj1@alu.ua.es>
2019-12-09 14:47:03 +0100

    Create About.vue
    Agreagado el about, falta meterle un mapa y algún retoque.

---
+++ resources/js/components/About.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col s12">
+                <article id="content">
+                    <header style="padding-left:0px;">
+                        <h2>Sobre nosotros</h2>
+                    </header>
+                    <div>
+                        <div>
+                            <p> 
+                                Bienvenido a UASport, su fuente número uno para todo tipo de productos deportivos. 
+                            </p>
+                            <p>
+                                Estamos dedicados a brindarle lo mejor en productos deportivos, con un enfoque en [característica de tienda 1], [característica de tienda 2], [característica de tienda 3].
+                            </p>
+                            <p>
+                                Nuestra empresa ha sido fundada en 2019 por [nombre del fundador], UASport ha recorrido un largo camino desde sus inicios en los mercadillos de alicante. 
+                            </p>
+                            <p>
+                                Cuando [nombre del fundador] comenzó, su pasión por los productos de deporte lo llevaron a dejar el trabajo diario, investigar mucho, buscar representantes y conseguir proveedores confiables para que UASport pueda ofrecerle lo último en productos de deporte. 
+                            </p>
+                            <p>
+                                Ahora servimos a clientes en toda España, y estamos encantados de poder convertir nuestra pasión por los productos de deporte en este sitio web y de poder compartir nuestra ilusión con gente como tu.
+                            </p>
+                            <p>
+                                Nosotros esperamos que encuentres lo que buscas en nuestro catálogo de productos y que disfrutes haciendo deporte con ellos tanto como nosotros disfrutamos ofreciéndolos. Si tiene alguna pregunta o comentario, no dude en comunicarse con nosotros a traves del teléfono [telefono] o en nuestras sucursales.
+                            </p>
+                            <p>
+                                Sinceramente,
+                            </p>
+                            <p>
+                                    [nombre del fundador]
+                            </p>
+                        </div>
+                    </div>
+                </article>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
