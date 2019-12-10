3fd346ea9165286f28e50b10bc6fde7d1c4a7afd
ecb34 <ecb34@alu.ua.es>
2019-12-10 12:58:29 +0100

    hecho vista tiendas y pulido detalles

---
+++ resources/js/components/Contacto.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-container>
+      <v-row>
+        <v-col cols="12" sm="6">
+            <h1>Puedes contactar con nosotros desde nuestras redes sociales</h1>
+            <h3 class="mt-3">Twitter: @uasport</h3>
+            <h3 class="mt-3">Facebook: UASPORT</h3>
+            <h3 class="mt-3">Instagram: @uasportes</h3>
+            <h3 class="mt-3">Teléfono de atención: 915938888</h3>
+        </v-col>
+        <v-col cols="12" sm="6">
+            <about></about>
+        </v-col>
+      </v-row>
+  </v-container>
+</template>
+
+<script>
+import About from "./About"
+export default {
+    components: {
+        "about": About,
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ resources/js/components/Tiendas.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-container>
+      <v-list>
+        <v-subheader color="primary" class="blue--text">LISTADO DE TIENDAS</v-subheader>
+        <v-list-item-group v-model="item" color="primary">
+          <v-list-item v-for="(item, i) in items" :key="i">
+            <v-list-item-content>
+              <v-list-item-title v-html="item" v-on:click="mostrarMapa(item)"></v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+      <v-label>{{itemSelected}}</v-label>
+  </v-container>
+</template>
+
+<script>
+export default {
+    data: () => ({
+      item: 4,
+      items: [
+          'Alicante',
+          'Ibi',
+          'Madrid',
+          'Torrevieja'
+      ],
+      itemSelected: ''
+    }),
+    methods: {
+        mostrarMapa(item){
+            console.log(item)
+            this.itemSelected = item
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
