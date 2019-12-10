d1fcd0c1730e210501168b1f5425748e0a34529a
ecb34 <ecb34@alu.ua.es>
2019-12-10 18:19:54 +0100

    añadido categorias recomendadas

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -1,6 +1,5 @@
 <template>
 <v-container>
-  <v-row>
     <v-carousel>
       <v-carousel-item v-for="(item,i) in items"
         :key="i"
@@ -9,18 +8,24 @@
         transition="fade-transition"
       ></v-carousel-item>
     </v-carousel>
-  </v-row>
-  
-  <v-row>
-    <h2 class="mt-3">Categorias Recomendadas</h2>
 
-  </v-row>
+  <h2 class="mt-3">Categorias Recomendadas</h2>
+  <carousel class="mt-5" :nav="false" :items="4" :loop="true">
+      <img src="https://placeimg.com/500/500/any?1">
+      <img src="https://placeimg.com/500/500/any?2">
+      <img src="https://placeimg.com/500/500/any?3">
+      <img src="https://placeimg.com/500/500/any?4">
+      <img src="https://placeimg.com/500/500/any?5">
+      <img src="https://placeimg.com/500/500/any?6">
+    </carousel>
 </v-container>
 </template>
 
 <script>
+import carousel from 'vue-owl-carousel'
 export default {
-data () {
+  components: { carousel },
+  data () {
       return {
         items: [
           {
