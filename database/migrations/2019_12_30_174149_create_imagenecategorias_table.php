b6183f17d082c6f20f4b38785a49ac2a32a066b8
juanbeteta <jmbt5@alu.ua.es>
2019-12-30 18:54:48 +0100

    resuelto error relacion imagenes categoria

--- database/migrations/2019_12_30_174149_create_imagenecategorias_table.php
+++ database/migrations/2019_12_30_174149_create_imagenecategorias_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCategoriasTable extends Migration
+class CreateImagenecategoriasTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,10 @@ class CreateCategoriasTable extends Migration
      */
     public function up()
     {
-        Schema::create('categorias', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->timestamps();
-        });
         Schema::table('categorias', function (Blueprint $table) 
         {
-            $table->integer('categoria_id')->nullable()->unsigned();
-            $table->foreign('categoria_id')->references('id')->on('categorias')->onDelete('cascade')->onUpdate('cascade');
+            $table->integer('imagen_id')->nullable()->unsigned();
+            $table->foreign('imagen_id')->references('id')->on('imagenes')->onDelete('cascade')->onUpdate('cascade');
         });
     }
 
