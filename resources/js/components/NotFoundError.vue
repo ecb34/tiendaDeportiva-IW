42702593ad6885d6ab8367009d8688ca096cdcf2
ecb34 <ecb34@alu.ua.es>
2020-01-01 16:19:42 +0100

    cambiada vista de paginas error

--- resources/js/components/NotFoundError.vue
+++ resources/js/components/NotFoundError.vue
@@ -1,5 +1,12 @@
 <template>
-  <h1>ERROR 404: Página no encontrada</h1>
+  <v-container>
+    <div class="container">
+      <h1 class="mt-12">ERROR 404</h1>
+      <h1 class="mt-8">Página no encontrada</h1>
+      <v-btn text class="mt-12" @click="$router.go(-1)" color="primary">Volver a la página anterior</v-btn>
+    </div>
+  </v-container>
+
 </template>
 
 <script>
@@ -9,5 +16,7 @@ export default {
 </script>
 
 <style>
-
+  .container{
+    height: 400px;
+  }
 </style>
--- resources/js/components/UnauthorizedError.vue
+++ resources/js/components/UnauthorizedError.vue
@@ -1,5 +1,11 @@
 <template>
-  <h1>ERROR 401: Usuario no autorizado</h1>
+  <v-container>
+    <div class="container">
+      <h1 class="mt-12">ERROR 401</h1>
+      <h1 class="mt-8">Usuario no autorizado</h1>
+      <v-btn text class="mt-12" @click="$router.go(-1)" color="primary">Volver a la página anterior</v-btn>
+    </div>
+  </v-container>
 </template>
 
 <script>
