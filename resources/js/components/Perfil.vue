95fc9c4a06f357e41785747cc2a1911605d277dd
Brian Methias Pesci Juliani <bmpj1@alu.ua.es>
2020-01-03 20:52:02 +0100

    Perfil retocado.

--- resources/js/components/Perfil.vue
+++ resources/js/components/Perfil.vue
@@ -4,19 +4,20 @@
       <div class="col-md-12">
         <div class="card card-widget widget-user">
           <!-- Add the bg color to the header using any of the bg-* classes -->
-          <div class="widget-user-header text-white" style="background-image:url('./images/img(11).jpg'); background-size:cover; background-position:center; border-radius:20%;">
+          <div class="widget-user-header text-white" style="background-image:url('./images/perfilBackground.jpg'); background-size:cover; background-position:center; border-radius:20%;">
             <h3 class="widget-user-username text-center" style="margin-left:1%;">{{user.nombre}} {{user.apellido}}</h3>
             <h5 class="widget-user-desc text-center" style="margin-left:1%;">{{user.rol}}</h5>
             
             <div class="widget-user-image text-center">
               <img class="img-circle border-right" style="border-radius:50%; margin-left:1%;" alt="User Avatar" src="images/noImagenPerfil.png">
             </div>
-                  <h4 class="widget-user-email text-center" style="color:white; margin-left:1%;">Email: {{user.email}}</h4>
-                  <h4 class="widget-user-email text-center" style="color:white; margin-left:1%;">Nacido el: {{user.fecha_nacimiento}}</h4>
+                  <h4 class="widget-user-email text-center" style="color:azure; margin-left:1%;">Email: {{user.email}}</h4>
+                  <h4 class="widget-user-email text-center" style="color:azure; margin-left:1%;">Telefono: {{user.telefono}}</h4>
+                  <h4 class="widget-user-email text-center" style="color:azure; margin-left:1%;">Nacido el: {{user.fecha_nacimiento}}</h4>
           </div>
           <div class="card-footer">
             <div class="row">
-              <div class="col-4 border">
+              <div class="col-4 col-4" style="border-right: 0.1em dotted gray">
                 <div class="description-block text-center">
                   <h5 class="description-header">{{totalGastado}}€</h5>
                   <span class="description-text">Compras</span>
@@ -24,7 +25,7 @@
                 <!-- /.description-block -->
               </div>
               <!-- /.col -->
-              <div class="col-4 border">
+              <div class="col-4 col-4" style="border-right: 0.1em dotted gray">
                 <div class="description-block text-center">
                   <h5 class="description-header">{{totalPedidos}}</h5>
                   <span class="description-text">Pedidos</span>
@@ -33,7 +34,7 @@
                 <!-- /.description-block -->
               </div>
               <!-- /.col -->
-              <div class="col-4 border">
+              <div class="col-4">
                 <div class="description-block text-center">
                   <h5 class="description-header">{{totalArticulos}}</h5>
                   <span class="description-text">Productos</span>
